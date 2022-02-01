5f1da428e7cdad2cdadbc5c18e4f718760666678
Jackson Almeida <jacksonadh@gmail.com>
2022-02-01 15:14:19 -0300

    personaizado card de repositorios

--- src/components/cardRepo/style.js
+++ src/components/cardRepo/style.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const CardRepo = styled.div`
   border-radius: 5px;
+  background: ${props => props.theme.backgroundPrimary};
   box-shadow: 0px 30px 40px -20px ${props => props.theme.shadow};
   padding: 30px;
   margin: 20px;
